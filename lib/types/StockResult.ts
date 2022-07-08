b958e7a2b8b1ff94e87eb2e0bd12d20f91a1b3f7
Young Jun Kim <yjkim10@hist.co.kr>
2022-07-08 17:29:16 +0900

    schema 정리

--- lib/types/StockResult.ts
+++
@@ -1,9 +0,0 @@
-import { StockInfo, TradePlan, NotifyItem } from '@prisma/client'
-
-type StockResult = {
-  stockInfo?: StockInfo
-  tradePlan?: TradePlan
-  notifyItem?: NotifyItem
-}
-
-export default StockResult
--- pages/api/plan.ts
+++ pages/api/plan.ts
@@ -1,32 +1,44 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
-import { StockInfo } from '@prisma/client'
+import { TradePlan, MarketType, AlarmCondition } from '@prisma/client'
 import prisma from '../../lib/prisma'
-import StockResult from '../../lib/types/StockResult'
 
-export default async function handler(req: NextApiRequest, res: NextApiResponse<StockResult>) {
+export default async function handler(req: NextApiRequest, res: NextApiResponse<TradePlan>) {
   const stockCode = ''
   const stockName = ''
 
   if (req.method === 'GET') {
-    const rsStockInfo = await prisma.stockInfo.findUnique({
-      where: {
-        stockCode,
-      },
-    })
-    const rsTradePlan = await prisma.tradePlan.findUnique({
-      where: {
-        stockCode,
-      },
-    })
-    const rsNotifyItem = await prisma.notifyItem.findUnique({
+    const result = await prisma.tradePlan.findUnique({
       where: {
         stockCode,
       },
     })
 
-    res.status(200).json({ stockInfo: rsStockInfo, tradePlan: rsTradePlan, notifyItem: rsNotifyItem })
+    res.status(200).json(result)
   } else if (req.method === 'POST') {
+    const result = await prisma.tradePlan.create({
+      data: {
+        stockCode: stockCode,
+        stockName: stockName,
+        marketType: MarketType.KOSPI,
+        tradeCategory: 'BS',
+        baseCandleOpenPrice: 0,
+        baseCandleClosePrice: 0,
+        baseCandleCenterPrice: 0,
+        baseCandleSize: 0,
+        tickPrice: 0,
+        transactionAmount: 0,
+        totalAssignedFund: 0,
+        currentAvgPrice: 0,
+        currentQuantity: 0,
+        currentTotalMoney: 0,
+        targetPrice: 0,
+        targetPriceAlarmCondition: AlarmCondition.LT,
+        enableTargetPriceAlarm: true,
+        enableVixAlarm: true,
+      },
+    })
+
     res.status(200).json({})
   } else if (req.method === 'DELETE') {
     res.status(200).json({})
